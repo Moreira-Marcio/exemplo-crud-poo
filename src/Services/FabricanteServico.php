95e7f328c7561f0f66fc333eed5bf08f636005ef
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-05-07 10:03:57 -0300

    atualiza conexoes

--- src/Services/FabricanteServico.php
+++ src/Services/FabricanteServico.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 
 namespace ExemploCrud\Services;
 
-use ExemploCrud\ConexaoBD;
+use ExemploCrud\Database\ConexaoBD;
 use Exception;
 use PDO;
 use Throwable;
@@ -11,20 +11,22 @@ final  class FabricanteServico
 {
     private PDO $conexao;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->conexao = ConexaoBD::getConexao();
     }
 
-    public function listarTodos():array {
-        $sql = "SELECT * FROM fabricantes WHERE id = :id";
+    public function listarTodos(): array
+    {
+        $sql = "SELECT * FROM fabricantes ORDER BY nome";
 
         try {
             $consulta = $this->conexao->prepare($sql);
             $consulta->execute();
-    
-            return $consulta->fetch(PDO::FETCH_ASSOC);
+
+            return $consulta->fetchAll(PDO::FETCH_ASSOC);
         } catch (Throwable $erro) {
-            throw new Exception("Erro ao carregar fabricante: ".$erro->getMessage());
-        }  
+            throw new Exception("Erro ao carregar fabricante: " . $erro->getMessage());
+        }
     }
 }
