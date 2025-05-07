4c0fd4df23d1aa6d5c66ec0fb1eabba0b5fd1741
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-05-07 11:14:08 -0300

    conclui inserir fabricante

--- fabricantes/inserir.php
+++ fabricantes/inserir.php
@@ -1,18 +1,20 @@
 <?php
-/* Verificando se o formulário foi acionado */
+
+use ExemploCrud\Models\Fabricante;
+use ExemploCrud\Services\FabricanteServico;
+
+require_once "../vendor/autoload.php";
+//em verde classe de serviço
+$fabrcanteServico = new FabricanteServico();
+
 if( isset($_POST['inserir']) ){
-
-    // Acessando as funções do CRUD de Fabricantes
-    require_once "../src/funcoes-fabricantes.php";
-
-    // Capturando o nome digitado do novo fabricante
-    $nome = filter_input(
-        INPUT_POST, "nome", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
-    // Inserindo o novo fabricante através de uma função
-    inserirFabricante($conexao, $nome);
-
-    /* Redirecionando para a visualização atualizada */
+    
+    
+    $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    //em verde a classe de modelo
+    $fabricante = new Fabricante($nome);   
+    $fabrcanteServico->inserir($fabricante);
+    
     header("location:visualizar.php");
     exit;
 }
